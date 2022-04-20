80ab89f8ffb2cb482b32a718ffeb78b14a6f3952
NaeLike เนไลก์ <87599521+Naelike@users.noreply.github.com>
2022-04-20 13:40:19 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("Number");
+console.log(Number("12"));
+console.log(Number("-1"));
+console.log(Number("3.4567"));
+
+console.log("parseInt");
+console.log(parseInt("10"));
+console.log(parseInt("10.5"));
+console.log(parseInt("1.2345"));
+
+console.log("parseFloat");
+console.log(parseFloat("3.14"));
+console.log(parseFloat("10E5"));
+console.log(parseFloat("-1.2345"));
